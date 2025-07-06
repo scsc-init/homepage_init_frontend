bbe070a7ed106f2778c0779d44233df67fe040bf
HK Lee <69958270+tteokgook1g@users.noreply.github.com>
2025-07-06 16:41:20 +0900

    chore: remove redundant comment

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,6 @@ const nextConfig = {
     });
     return config;
   },
-
-  // async rewrites() {
-  //   const backendUrl = process.env.BACKEND_URL;
-
-  //   if (!backendUrl) {
-  //     throw new Error("BACKEND_URL is not defined in environment variables");
-  //   }
-
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `${backendUrl}/api/:path*`,
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
