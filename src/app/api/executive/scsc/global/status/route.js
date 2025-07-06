ee66430546b8b2a8655540a469fe98e55672d757
HK Lee <69958270+tteokgook1g@users.noreply.github.com>
2025-07-06 14:25:38 +0900

    update: server side fetch of ScscStatusPanel

---
+++ src/app/api/executive/scsc/global/status/route.js
@@ -0,0 +1,20 @@
+import { headers } from "next/headers";
+import { getBaseUrl } from "@/util/getBaseUrl";
+import { getApiSecret } from "@/util/getApiSecret";
+
+export async function POST(request) {
+  const headersList = headers();
+  const jwt = headersList.get("x-jwt");
+  const body = await request.json();
+  const res = await fetch(`${getBaseUrl()}/api/executive/scsc/global/status`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-secret": getApiSecret(),
+      "x-jwt": jwt,
+    },
+    body: JSON.stringify(body),
+    cache: "no-store",
+  });
+  return res;
+}
