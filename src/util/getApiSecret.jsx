8b30d2206cc6387cb5ad4215ab3aed2ce70b7690
1n1tial <dan.yun0821@gmail.com>
2025-07-13 12:19:58 +0900

    fix: remove NEXT_PUBLIC_

--- src/util/getApiSecret.jsx
+++ src/util/getApiSecret.jsx
@@ -1,7 +1,3 @@
 export function getApiSecret() {
-  if (typeof window === "undefined") {
-    return process.env.API_SECRET || "";
-  } else {
-    return process.env.NEXT_PUBLIC_API_SECRET || "";
-  }
+  return process.env.API_SECRET || "";
 }
--- src/util/getBaseUrl.jsx
+++ src/util/getBaseUrl.jsx
@@ -1,9 +1,3 @@
 export function getBaseUrl() {
-  if (typeof window === "undefined") {
-    // SSR
-    return process.env.BACKEND_URL ?? "http://localhost:8080";
-  } else {
-    // CSR
-    return process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8080";
-  }
+  return process.env.BACKEND_URL ?? "http://localhost:8080";
 }
