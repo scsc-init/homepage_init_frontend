9c0dc09cb3210e0fefb8ffebdd8377dce23086ae
1n1tial <dan.yun0821@gmail.com>
2025-07-21 12:26:24 +0900

    feat: add session reloading, editing, member fetching to pig

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -1,5 +1,5 @@
 import { handleApiRequest } from "@/app/api/apiWrapper";
 
-export async function GET({ params }) {
+export async function GET(request, { params }) {
   return handleApiRequest("GET", "/api/user/{id}", { params });
 }
