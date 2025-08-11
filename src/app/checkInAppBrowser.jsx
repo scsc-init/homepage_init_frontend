79827b8bc2c464718fa5e7df6fc21d606560e172
1n1tial <dan.yun0821@gmail.com>
2025-08-12 02:11:37 +0900

    ⚡️ add check kakao to main logo

--- src/app/checkInAppBrowser.jsx
+++ src/app/checkInAppBrowser.jsx
@@ -4,32 +4,39 @@ import "./page.css";
 import { useEffect, useState } from "react";
 
 const IN_APP_BROWSER_NAMES = {
-  "KAKAOTALK": "카카오톡"
+  "kakaotalk": "카카오톡",
+  "everytimeapp": "에브리타임",
+  "instagram": "인스타그램",
 }
 
-// export const checkInAppBrowser = () => {
-//   const [isInAppBrowser, setIsInAppBrowser] = useState(false);
-//   const [inAppBrowserName, setInAppBrowserName] = useState('');
-//   useEffect(() => {
-//     for (const [key, name] of Object.entries(IN_APP_BROWSER_NAMES)) {
-//       if (navigator.userAgent.match(key)) {setIsInAppBrowser(true); setInAppBrowserName(name); }
-//     }
-//   },[])
-//   return inAppBrowserName
-// }
+
+function InAppBrowserOutButton({ inAppBrowserName }) {
+  const onClick = () => {
+    if (inAppBrowserName==='kakaotalk') {
+      var target_url = 'https://homepage-init-frontend-ixxt.vercel.app/';
+      location.href = 'kakaotalk://web/openExternal?url='+encodeURIComponent(target_url);
+    }
+  }
+
+  return <button onClick={onClick}>외부 브라우저로 이동</button>
+};
 
 export const MainLogo = () => {
   const [isInAppBrowser, setIsInAppBrowser] = useState(false);
   const [inAppBrowserName, setInAppBrowserName] = useState('');
   useEffect(() => {
     for (const [key, name] of Object.entries(IN_APP_BROWSER_NAMES)) {
-      if (navigator.userAgent.match(key)) {setIsInAppBrowser(true); setInAppBrowserName(name); }
+      if (navigator.userAgent.toLowerCase().match(key)) {setIsInAppBrowser(true); setInAppBrowserName(name); }
     }
   },[])
   
   return (
     isInAppBrowser ? (
-      <div>{inAppBrowserName}</div>
+      <div className="main-logo-wrapper">
+        <h1><strong>{inAppBrowserName}</strong> 브라우저에서는 <strong>로그인</strong>이 실패할 수 있습니다</h1>
+        <p>인앱 브라우저가 아닌 <strong>Chrome, Safari, 삼성 인터넷</strong>등의 외부 브라우저를 이용하면 더 안전합니다.</p>
+        <InAppBrowserOutButton/>
+      </div>
     ) : (
       <div className="main-logo-wrapper">
         <img
