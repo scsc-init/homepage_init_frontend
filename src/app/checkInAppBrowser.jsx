e2c179a9318c52394e398a444765ad31acb83b74
1n1tial <dan.yun0821@gmail.com>
2025-08-12 01:35:18 +0900

    ⚡️ add check kakao to main logo

--- src/app/checkInAppBrowser.jsx
+++ src/app/checkInAppBrowser.jsx
@@ -7,16 +7,16 @@ const IN_APP_BROWSER_NAMES = {
   "KAKAOTALK": "카카오톡"
 }
 
-export const checkInAppBrowser = () => {
-  const [isInAppBrowser, setIsInAppBrowser] = useState(false);
-  const [inAppBrowserName, setInAppBrowserName] = useState('');
-  useEffect(() => {
-    for (const [key, name] of Object.entries(IN_APP_BROWSER_NAMES)) {
-      if (navigator.userAgent.match(key)) {setIsInAppBrowser(true); setInAppBrowserName(name); }
-    }
-  },[])
-  return inAppBrowserName
-}
+// export const checkInAppBrowser = () => {
+//   const [isInAppBrowser, setIsInAppBrowser] = useState(false);
+//   const [inAppBrowserName, setInAppBrowserName] = useState('');
+//   useEffect(() => {
+//     for (const [key, name] of Object.entries(IN_APP_BROWSER_NAMES)) {
+//       if (navigator.userAgent.match(key)) {setIsInAppBrowser(true); setInAppBrowserName(name); }
+//     }
+//   },[])
+//   return inAppBrowserName
+// }
 
 export const MainLogo = () => {
   const [isInAppBrowser, setIsInAppBrowser] = useState(false);
