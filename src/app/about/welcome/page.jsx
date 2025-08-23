c61a48db476f4cd9bdf6ca25939bb9018e13f335
박성현 <54495942+coshaman@users.noreply.github.com>
2025-08-23 14:59:48 +0900

    🚀 deploy v0.1.7-b (#149)
    * ⚡welcome 페이지 디자인 수정 / opengraph image fix (#138)
    
    * 계좌
    
    * discord link
    
    * hotfix
    
    * ⚡임원진 사진 수정 (#143)
    
    * 계좌
    
    * discord link
    
    * hotfix
    
    * executive profile img
    
    * ⚡welcome 페이지 링크 설정 개선 / 로그인 시그 hotfix (#144)
    
    * hotfix
    
    * hotfix
    
    * readme
    
    * hotfix
    
    * fix
    
    * info (#148)

--- src/app/about/welcome/page.jsx
+++ src/app/about/welcome/page.jsx
@@ -1,14 +1,21 @@
 import "./page.css";
 import { getBaseUrl } from "@/util/getBaseUrl";
 import { getApiSecret } from "@/util/getApiSecret";
-import { DEPOSIT_ACC, DISCORD_INVITE_LINK, KAKAO_INVITE_LINK } from "@/util/constants";
-import EnrollButton from "./EnrollButton"
+import {
+  DEPOSIT_ACC,
+  DISCORD_INVITE_LINK,
+  KAKAO_INVITE_LINK,
+} from "@/util/constants";
+import EnrollButton from "./EnrollButton";
 
 async function fetchDiscordInviteLink() {
-  const res = await fetch(`${getBaseUrl()}/api/bot/discord/general/get_invite`, {
-    headers: { "x-api-secret": getApiSecret() },
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${getBaseUrl()}/api/bot/discord/general/get_invite`,
+    {
+      headers: { "x-api-secret": getApiSecret() },
+      cache: "no-store",
+    },
+  );
   if (res.ok) {
     const resData = await res.json();
     return resData.result.invite_url;
@@ -35,14 +42,34 @@ export default async function Contact() {
             <p>자세한 설명은 <a href={discordInviteLink} target="_blank" rel="noopener noreferrer">디스코드 서버</a>에서 확인할 수 있습니다.</p>
             <hr></hr>
             수고하셨습니다. 가입이 완료되었습니다. SCSC에 온 걸 진심으로 환영합니다! */}
-            <h3><a href={DISCORD_INVITE_LINK} target="_blank" rel="noopener noreferrer">디스코드 서버 링크</a></h3>
-            <h3><a href={KAKAO_INVITE_LINK} target="_blank" rel="noopener noreferrer">카카오톡 팀 채팅방</a></h3>
+            <h3>
+              <a
+                href={DISCORD_INVITE_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                디스코드 서버 링크
+              </a>
+            </h3>
+            <h3>
+              <a
+                href={KAKAO_INVITE_LINK}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                카카오톡 팀 채팅방
+              </a>
+            </h3>
             <h3>{DEPOSIT_ACC}</h3>
             <div className="deposit-container">
-              <h3>회비: 2.5만원</h3>
-              <EnrollButton/>
+              <h3>
+                회비: 2.5만원 <br></br>입금자명 : 이름 + 전화번호 마지막 두자리{" "}
+                (ex 김창섭57)
+              </h3>
+
+              <EnrollButton />
             </div>
-            <a href='/about/my-page'>마이페이지로 이동</a>
+            <a href="/about/my-page">마이페이지로 이동</a>
           </div>
         </div>
       </div>
