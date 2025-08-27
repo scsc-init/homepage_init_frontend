d2a070183cbc27d3de6cb98df7a7ace5a7b04936
HK Lee <69958270+tteokgook1@users.noreply.github.com>
2025-08-27 16:32:26 +0900

    feat/bot-login

--- src/app/us/(auth)/login/page.jsx
+++ src/app/us/(auth)/login/page.jsx
@@ -1,5 +1,19 @@
+import { getBaseUrl } from "@/util/getBaseUrl";
 import AuthClient from "./AuthClient";
+import { getApiSecret } from "@/util/getApiSecret";
 
-export default function Page() {
+export default async function Page() {
+  const discordLogin = async () => {
+    const res = await fetch(`${getBaseUrl()}/api/bot/discord/login`, {
+      method: "POST",
+      headers: {
+        "x-api-secret": getApiSecret(),
+      },
+    });
+    if (res.status === 204) console.log("봇 로그인 성공");
+    else console.log(`봇 로그인 실패: ${await res.text()}`);
+  };
+  await discordLogin();
+
   return <AuthClient />;
 }
