9ddb00cab62b042a775bf57f4de7affdfd3dbccf
이태윤 <dlxodbs0908@gmail.com>
2025-09-29 00:02:58 +0900

    토글 버튼 수정

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,10 +1,18 @@
 import './ToggleSwitch.css';
 
-const ToggleSwitch = ({ checked, onChange }) => {
+const ToggleSwitch = ({ checked, onChange, className = '', ...rest }) => {
+  const value = !!checked;
+  const labelClass = ['switch switch--form', className].filter(Boolean).join(' ');
+
   return (
-    <label className="switch">
-      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
-      <span className="slider round"></span>
+    <label className={labelClass} title={value ? "켜짐" : "꺼짐"}>
+      <input
+        type="checkbox"
+        checked={value}
+        onChange={(event) => onChange?.(event.target.checked)}
+        {...rest}
+      />
+      <span className="slider" aria-hidden="true"></span>
     </label>
   );
 };
