a316bdfd7add0079ba8a810048450ddb9820e288
taeyoon0908 <ty0908@snu.ac.kr>
2025-09-29 16:52:17 +0900

    ⚡SigForm 컴포넌트 boolean 필드 디자인 (#228)
    * 집가서자자
    
    * sig강조효과
    
    * sig 강조효과 수정
    
    * pig 강조 효과 추가
    
    * 테두리 강조 수정
    
    * 11
    
    * 토글 버튼 수정
    
    * ddd
    
    ---------
    
    Co-authored-by: 이태윤 <dlxodbs0908@gmail.com>

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,11 +1,18 @@
 import './ToggleSwitch.css';
 
-const ToggleSwitch = ({ checked, onChange }) => {
+const ToggleSwitch = ({ checked, onChange}) => {
+  const value = !!checked;
+  const labelClass = ['switch switch--form', className].filter(Boolean).join(' ');
+
   return (
-    <label className="switch">
-      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
-      <span className="slider round"></span>
-    </label>
+    <label className={labelClass} title={value ? "켜짐" : "꺼짐"}>
+    <input
+      type="checkbox"
+      checked={value}
+      onChange={(event) => onChange?.(event.target.checked)}
+    />
+      <span className="slider" aria-hidden="true"></span>
+  </label>
   );
 };
 
