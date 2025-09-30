3f9ee94ff2cde9c82388d121600ed7cd0fd9182f
박성현 <54495942+coshaman@users.noreply.github.com>
2025-09-30 15:08:52 +0900

    ⚡ 로그인 hotfix(try except) (#238)
    * hotfix
    
    * hotfix
    
    * hotfix
    
    * hotfix

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -3,23 +3,15 @@
 
 import './ToggleSwitch.css';
 
-export default function ToggleSwitch({
-  checked,
-  value: valueProp,
-  onChange,
-  className = '',
-  ...rest
-}) {
+export default function ToggleSwitch({ checked, value: valueProp, onChange }) {
   const isOn = typeof checked !== 'undefined' ? !!checked : !!valueProp;
-  const labelClass = ['switch switch--form', className].filter(Boolean).join(' ');
 
   return (
-    <label className={labelClass} title={isOn ? '켜짐' : '꺼짐'}>
+    <label className="switch switch--form" title={isOn ? '켜짐' : '꺼짐'}>
       <input
         type="checkbox"
         checked={isOn}
         onChange={(e) => onChange?.(e.target.checked)}
-        {...rest}
       />
       <span className="slider" aria-hidden="true" />
     </label>
