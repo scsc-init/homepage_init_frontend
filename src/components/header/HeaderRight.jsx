20fbf64a8fd6af36261a3c95153a7f5dc6d264ca
coshaman <kmcha72@gmail.com>
2025-10-05 21:33:41 +0900

    ⚡ UX improvement

--- src/components/header/HeaderRight.jsx
+++ src/components/header/HeaderRight.jsx
@@ -24,7 +24,7 @@ export default function HeaderRight() {
       {user === null && (
         <div className="HeaderRight__login">
           <Link href={'/us/login'} id="HeaderUserLogin" className="unset">
-            동아리 가입 / 로그인
+            가입 / 로그인
           </Link>
         </div>
       )}
