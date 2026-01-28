3bbf8b09aa3ac13b27d12d821cfabffe4ae358af
Hankyung Lee <69958270+tteokgook1@users.noreply.github.com>
2026-01-28 19:51:07 +0900

    :sparkles: 테스트 유틸리티 (#380)
    * :sparkles: 테스트 유틸리티
    
    * feat: test user console
    
    * fix: major string
    
    * fix: reviews

---
+++ src/app/testutils/page.jsx
@@ -0,0 +1,18 @@
+import { ENABLE_TEST_UTILS } from '@/util/constants';
+import { notFound } from 'next/navigation';
+import NextauthConsole from './NextauthConsole';
+import UserConsole from './UserConsole';
+import styles from './page.module.css';
+
+export default function TestutilsPage() {
+  if (!ENABLE_TEST_UTILS) {
+    notFound();
+  }
+  return (
+    <main className={styles['main-page']}>
+      <h1>Test Utils Console</h1>
+      <NextauthConsole />
+      <UserConsole />
+    </main>
+  );
+}
