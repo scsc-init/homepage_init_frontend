a8f769d087957366c0eb8925faed717cf9378402
taeyoon0908 <ty0908@snu.ac.kr>
2026-02-02 20:38:07 +0900

    테스트 유틸 기능 수정 2차

--- src/app/testutils/page.jsx
+++ src/app/testutils/page.jsx
@@ -1,18 +1,9 @@
-import { ENABLE_TEST_UTILS } from '@/util/constants';
+﻿import { ENABLE_TEST_UTILS } from '@/util/constants';
 import { notFound } from 'next/navigation';
 import NextauthConsole from './NextauthConsole';
 import UserConsole from './UserConsole';
-import Link from 'next/link';
+import ManagementLinks from './ManagementLinks';
 import styles from './page.module.css';
-
-const MANAGEMENT_SECTIONS = [
-  { title: '유저 관리', href: '/executive/user' },
-  { title: 'HTML 관리', href: '/executive/w' },
-  { title: '게시글 관리', href: '/executive/board' },
-  { title: 'sig 관리', href: '/executive/sig' },
-  { title: 'pig 관리', href: '/executive/pig' },
-  { title: 'KV 테이블 관리', href: '/executive/kv' },
-];
 
 export default function TestutilsPage() {
   if (!ENABLE_TEST_UTILS) {
@@ -21,17 +12,9 @@ export default function TestutilsPage() {
   return (
     <main className={styles['main-page']}>
       <h1>Test Utils Console</h1>
+      <ManagementLinks />
       <NextauthConsole />
       <UserConsole />
-      {MANAGEMENT_SECTIONS.map(({ title, href }) => (
-        <section key={href} className="adm-section">
-          <div className="adm-actions">
-            <Link href={href} className="adm-button outline">
-              {title}
-            </Link>
-          </div>
-        </section>
-      ))}
     </main>
   );
 }
