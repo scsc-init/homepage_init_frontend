04af8d3e2cb542b4904413d1c1035959f892aa54
coshaman <kmcha72@gmail.com>
2026-03-19 13:53:34 +0900

    wip

--- src/app/api/sig/category/[tag_text]/route.js
+++
@@ -1,7 +0,0 @@
-import { handleApiRequest } from '@/app/api/apiWrapper';
-
-export async function GET(request, { params }) {
-  const { searchParams } = new URL(request.url);
-  const query = Object.fromEntries(searchParams.entries());
-  return handleApiRequest('GET', '/api/sig/category/{tag_text}', { params, query });
-}
