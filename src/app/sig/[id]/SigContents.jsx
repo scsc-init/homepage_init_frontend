b15ddf05d3ac752797849e197ffade2b2f4cadda
Yu Jungkyun <jungk612@naver.com>
2026-03-28 16:41:19 +0900

    Fix: Adjust Sig image size with img tag

--- src/app/sig/[id]/SigContents.jsx
+++ src/app/sig/[id]/SigContents.jsx
@@ -18,6 +18,9 @@ export default function SigContents({ content }) {
           li: ({ _node, ...props }) => <li className="mdx-li" {...props} />,
           code: ({ _node, ...props }) => <code className="mdx-inline-code" {...props} />,
           pre: ({ _node, ...props }) => <pre className="mdx-pre" {...props} />,
+          img: ({ _node, ...props }) => (
+            <img className="mdx-img" {...props} alt="article image" />
+          ),
         }}
       >
         {content || ''}
