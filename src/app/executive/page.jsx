1000a6fe1db01bb92df803a1bb5209b1d523f2de
유정균 <154410066+jungk612@users.noreply.github.com>
2026-04-04 12:48:42 +0900

    ✨ 임원진 페이지 대시보드 디자인 적용 & 경고 문구 추가 (#514)
    * feat: 임원진 페이지 대시보드 디자인 적용 & 경고 문구 추가
    - 임원진 페이지 대시보드 디자인 구현
    - SCSC status 관리 영역에 붉은색 테두리와 붉은색 경고 문구 추가
    
    * fix: fix card transition
    
    * fix: handle failed SCSC Status fetch
    - SCSCGlobalStatus fetch에 실패했을 때 EnrollmentPolicyPanel에서 런타임 에러가 나는 잠정적인 에러를 해결
    - fetch 실패 시 '상태를 불러오지 못했습니다'를 출력하도록 수정

--- src/app/executive/page.jsx
+++ src/app/executive/page.jsx
@@ -6,59 +6,89 @@ import EnrollmentPolicyPanel from './EnrollmentPolicyPanel';
 import { fetchSCSCGlobalStatus, fetchDiscordBotStatus } from '@/util/fetchAPIData';
 import './page.css';
 
+const adminPageLinks = [
+  {
+    title: '유저 관리',
+    description: '임원진 및 유저 관리 페이지로 이동',
+    href: '/executive/user',
+  },
+  {
+    title: '지원금 요청',
+    description: '지원금 요청 게시판으로 이동',
+    href: '/board/6',
+  },
+  {
+    title: 'HTML 페이지 관리',
+    description: 'HTML 페이지 관리 페이지로 이동',
+    href: '/executive/w',
+  },
+  {
+    title: '게시글 관리',
+    description: '게시글 관리 페이지로 이동',
+    href: '/executive/board',
+  },
+  {
+    title: 'SIG 관리',
+    description: 'SIG 관리 페이지로 이동',
+    href: '/executive/sig',
+  },
+  {
+    title: 'PIG 관리',
+    description: 'PIG 관리 페이지로 이동',
+    href: '/executive/pig',
+  },
+  {
+    title: 'KV table 관리',
+    description: 'KV table 관리 페이지로 이동',
+    href: '/executive/kv',
+  },
+  {
+    title: '전공 관리',
+    description: '전공 목록 페이지로 이동',
+    href: '/executive/major',
+  },
+];
+
 export default async function AdminPanel() {
   const [scscGlobalStatus, discordBotStatus] = await Promise.allSettled([
     fetchSCSCGlobalStatus(),
     fetchDiscordBotStatus(),
   ]);
+  const resolvedScscGlobalStatus =
+    scscGlobalStatus.status === 'fulfilled' ? scscGlobalStatus.value : null;
 
   return (
     <WithAuthorization>
       <div className="admin-panel">
-        <h2>유저 관리</h2>
-        <p>
-          <Link href="/executive/user">임원진 및 유저 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>지원금 요청</h2>
-        <p>
-          <Link href="/board/6">지원금 요청 게시판으로 이동</Link>
-        </p>
-
-        <h2>HTML 페이지 관리</h2>
-        <p>
-          <Link href="/executive/w">HTML 페이지 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>게시글 관리</h2>
-        <p>
-          <Link href="/executive/board">게시글 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>SIG 관리</h2>
-        <p>
-          <Link href="/executive/sig">SIG 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>PIG 관리</h2>
-        <p>
-          <Link href="/executive/pig">PIG 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>SCSC status 관리</h2>
-        <div className="adm-section">
-          <EnrollmentPolicyPanel scscGlobalStatus={scscGlobalStatus.value} />
+        <div className="admin-page-list">
+          {adminPageLinks.map(({ title, description, href }) => {
+            return (
+              <Link key={title} href={href} className="admin-page-card">
+                <div className="admin-page-card-content">
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                </div>
+              </Link>
+            );
+          })}
         </div>
-        <div className="adm-section">
-          <ScscStatusPanel
-            scscGlobalStatus={
-              scscGlobalStatus.status === 'fulfilled' ? scscGlobalStatus.value.status : null
-            }
-            semester={
-              scscGlobalStatus.status === 'fulfilled' ? scscGlobalStatus.value.semester : null
-            }
-            year={scscGlobalStatus.status === 'fulfilled' ? scscGlobalStatus.value.year : null}
-          />
+
+        <div className="dangerous-area">
+          <span className="warning-text">
+            * 본 영역은 시스템에 대한 직접적인 영향을 미칠 수 있는 기능을 포함하고 있습니다.
+            조작 시 주의해주세요. *
+          </span>
+          <h2>SCSC status 관리</h2>
+          <div className="adm-section">
+            <EnrollmentPolicyPanel scscGlobalStatus={resolvedScscGlobalStatus} />
+          </div>
+          <div className="adm-section">
+            <ScscStatusPanel
+              scscGlobalStatus={resolvedScscGlobalStatus?.status}
+              semester={resolvedScscGlobalStatus?.semester}
+              year={resolvedScscGlobalStatus?.year}
+            />
+          </div>
         </div>
 
         <h2>디스코드 봇 관리</h2>
@@ -69,17 +99,6 @@ export default async function AdminPanel() {
             }
           />
         </div>
-
-        <h2>KV table 관리</h2>
-        <p>Footer 등 사이트 수정은 이곳에서 해주세요.</p>
-        <p>
-          <Link href="/executive/kv">KV table 관리 페이지로 이동</Link>
-        </p>
-
-        <h2>전공 관리</h2>
-        <p>
-          <Link href="/executive/major">전공 목록 페이지로 이동</Link>
-        </p>
       </div>
     </WithAuthorization>
   );
