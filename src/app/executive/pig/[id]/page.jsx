1840134595619dca7dfb292d5553aa01ea199936
Yu Jungkyun <jungk612@naver.com>
2026-04-26 03:23:07 +0900

    fix: fixed executive pig page runtime error

--- src/app/executive/pig/[id]/page.jsx
+++ src/app/executive/pig/[id]/page.jsx
@@ -14,16 +14,8 @@ export default async function ExecutivePigPage({ params }) {
     return null;
   }
 
-  const [pigMembers, pigArticle] = await Promise.all([
-    safeFetch('GET', `/api/pig/${pigMeta.value.id}/members`),
-    safeFetch('GET', `/api/article/${pigMeta.value.content_id}`),
-  ]);
-
-  const pig = {
-    ...pigMeta.value,
-    content: pigArticle?.content ?? '',
-    members: Array.isArray(pigMembers) ? pigMembers : [],
-  };
+  const pig = pigMeta.value;
+  pig.content = pig.content.content;
 
   return (
     <WithAuthorization>
