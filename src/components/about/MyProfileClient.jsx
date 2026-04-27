c7bb563f8c38ad55be03f185f34f105c92da0cf0
mickey0605 <mickeylee1698@gmail.com>
2026-04-27 11:50:41 +0900

    아마 최종

--- src/components/about/MyProfileClient.jsx
+++ src/components/about/MyProfileClient.jsx
@@ -10,8 +10,7 @@ import './myProfile.css';
 import { MainLogoImage } from '@/components/common/MainLogoImage';
 import { FaDiscord } from 'react-icons/fa';
 import { AiOutlineMessage } from 'react-icons/ai';
-import { MdArrowOutward, MdOutlineInfo } from 'react-icons/md';
-import Image from 'next/image';
+import { MdArrowOutward, MdOutlineInfo, MdLogout } from 'react-icons/md';
 
 const USER_ROLE_MAP = {
   0: '최저권한',
@@ -103,17 +102,13 @@ export default function MyProfileClient() {
       </div>
 
       <div className="user-profile-wrapper">
-        {user ? (
-          <Image
-            src={user.profile_picture || '/asset/default-pfp.png'}
-            alt="Profile"
-            className="user-profile-picture"
-            width={50}
-            height={50}
-          />
-        ) : (
-          <Image alt="profile" height={50} width={50} src="/asset/default-pfp.png" />
-        )}
+        <img
+          src={user?.profile_picture || '/asset/default-pfp.png'}
+          alt="Profile"
+          width={50}
+          height={50}
+          className="user-profile-picture"
+        />
         <div className="user-name-container">
           <div className="user-name">
             {user ? `${user.name} [${USER_ROLE_MAP[user.role]}]` : ''}
@@ -202,6 +197,16 @@ export default function MyProfileClient() {
               </span>
               <span className="btn-label">정보 수정</span>
             </a>
+            <button
+              onClick={handleLogout}
+              className="action-button"
+              style={{ cursor: 'pointer', border: 'none' }}
+            >
+              <span className="btn-icon">
+                <MdLogout size="24" />
+              </span>
+              <span className="btn-label">로그아웃</span>
+            </button>
           </div>
         </div>
       </div>
