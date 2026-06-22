73c6e28caab411f76c2c799f58ccdac9466e12b4
winteruss <melonmatheyol@gmail.com>
2026-06-22 18:49:11 +0900

    Deleting route.ts file for src/app/api/comments

--- src/app/api/comments/[id]/executive/delete/route.js
+++
@@ -1,5 +0,0 @@
-import { handleApiRequest } from '@/app/api/apiWrapper';
-
-export async function POST(request, { params }) {
-  return handleApiRequest('POST', '/api/executive/comment/delete/{id}', { params });
-}
