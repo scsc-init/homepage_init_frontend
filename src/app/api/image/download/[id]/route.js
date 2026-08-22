f9175a653b08c71bd9d8b1bc576ea5e26bf6dde9
Hankyung Lee <69958270+tteokgook1@users.noreply.github.com>
2026-08-22 13:15:55 +0900

    :bug: fix image redirect (#764)

--- src/app/api/image/download/[id]/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request, { params }) {
-  const resolvedParams = await params;
-  const id = encodeURIComponent(resolvedParams.id);
-  const targetUrl = new URL(`/api/file/image/download/${id}`, request.url);
-
-  return NextResponse.redirect(targetUrl, 307);
-}
