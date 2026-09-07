205361a1365fa6f9dedd6c7946310378f3ee96c5
mickey0605 <mickeylee1698@gmail.com>
2026-09-07 11:02:41 +0900

    1차

--- src/app/us/(auth)/login/page.jsx
+++ src/app/us/(auth)/login/page.jsx
@@ -1,6 +1,16 @@
 import AuthClient from './AuthClient';
+import { fetchBackendServer } from '@/util/fetch/server';
 
 export default async function LoginPage(props) {
+  const discordLogin = async () => {
+    const res = await fetchBackendServer('POST', '/api/bot/discord/login', {
+      headers: { 'x-api-secret': process.env.API_SECRET || '' },
+    });
+    if (res.status === 204) console.log('봇 로그인 성공');
+    else console.log(`봇 로그인 실패: ${await res.text()}`);
+  };
+  discordLogin();
+
   const searchParams = await props.searchParams;
 
   const redirectTo =
